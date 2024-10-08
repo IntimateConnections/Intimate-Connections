8c3be7fc92483b4baa72ba06082cf1ca11d0c718
IntimateConnections <treasuremoreshopp@gmail.com>
2024-10-07 20:57:50 -0700

    Create Script .js
    Initial file for website

---
+++ js/Script .js
@@ -0,0 +1,14 @@
+document.getElementById("connectBtn").addEventListener("click", function() {
+    alert("Redirecting to chat platforms...");
+    window.location.href = "https://examplechatplatform.com"; // Use your
+link here
+});
+
+const exploreButtons = document.querySelectorAll(".exploreBtn");
+exploreButtons.forEach(button => {
+    button.addEventListener("click", function() {
+        alert("Taking you to the chat site...");
+        window.location.href = "https://examplechatsite.com"; // Use your
+affiliate links here
+    });
+});
